6adad6746b6b52387efeddcb9a8b8904ffc875ae
tirupaalepu <tirupaalepu@yahoo.com>
2022-09-16 04:14:31 -0400

    2dn

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
